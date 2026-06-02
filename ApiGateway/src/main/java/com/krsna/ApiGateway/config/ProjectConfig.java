8ae8022f418ce4f86b0accf980ae6b462d252f3f
ys Asesa Nitaicandra <abinashshaw16@gmail.com>
2026-06-02 21:28:26 +0530

    Implemented Rate Limiter using Redis-Rate-Limiter implementing Token-Bucket-Algorithm where the key is the header value user to identify request

--- ApiGateway/src/main/java/com/krsna/ApiGateway/config/ProjectConfig.java
+++ ApiGateway/src/main/java/com/krsna/ApiGateway/config/ProjectConfig.java
@@ -1,11 +1,14 @@
 package com.krsna.ApiGateway.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -33,20 +36,20 @@ public class ProjectConfig {
         RouteLocator myRoutes = builder.routes().
                 route( "user-route", r ->
                         r.path("/users/**")
-                                 .filters(f -> f
-                                .circuitBreaker(c -> c.setName("UserCircuitBreaker").setFallbackUri("forward:/fallback-call"))
-                                .retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET, HttpMethod.POST)
+                                 .filters(f -> f.requestRateLimiter(config -> config.setKeyResolver(getKeyResolver()).setRateLimiter(redisRateLimiter()))
+                                        .circuitBreaker(c -> c.setName("UserCircuitBreaker").setFallbackUri("forward:/fallback-call"))
+                                        .retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET, HttpMethod.POST)
                                         .setBackoff(Duration.ofMillis(100), Duration.ofMillis(10000), 2, true))
                                  .rewritePath("/users/?(?<remaining>.*)", "/${remaining}"))
                                  .uri("lb://"+userServiceId))
 
-                    .route("hotel-route", r ->
+                .route("hotel-route", r ->
                         r.path("/hotels/**")
                                 .filters(f -> f.circuitBreaker(c -> c.setName("HotelCircuitBreaker").setFallbackUri("forward:/fallback-call"))
                                 .rewritePath("/hotels/?(?<remaining>.*)", "/${remaining}"))
                                 .uri("lb://"+hotelServiceId))
 
-                    .route("rating-route", r ->
+                .route("rating-route", r ->
                         r.path("/ratings/**")
                                 .filters(f -> f.circuitBreaker(c -> c.setName("RatingCircuitBreaker").setFallbackUri("forward:/fallback-call"))
                                 .rewritePath("/ratings/?(?<remaining>.*)", "/${remaining}"))
@@ -54,5 +57,17 @@ public class ProjectConfig {
                 .build();
 
         return myRoutes;
+    }
+
+    @Bean
+    public KeyResolver getKeyResolver(){
+        return exchange -> Mono.just(exchange.getRequest().getHeaders().getFirst("user"));
+        // Please don't forget to set the header user while sending request
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter(){
+        return new RedisRateLimiter(4, 4, 1);
+        // Allowing 4 requests per second per user
     }
 }
---
+++ ApiGateway/src/main/resources/notes.md
@@ -0,0 +1,3 @@
+```bash
+docker run -d --name redis-rate-limiter -p 6379:6379 redis:latest
+```
